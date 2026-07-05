3b0b6c1ffe794d53c934cb1a9b2ea8606c9ac724
Harald Kapper <4014716+hknet@users.noreply.github.com>
2026-07-05 15:40:43 +0200

    Release 0.84.1.42 beta

--- version.h
+++ version.h
@@ -8,6 +8,6 @@
  * default stuff used for local development runs of 'make'.
  */
 
-#define TEXTVER "0.84.1.41-beta (KiTTY, PuTTY 0.84 base)"
+#define TEXTVER "0.84.1.42-beta (KiTTY, PuTTY 0.84 base)"
 #define SSHVER "_Release_0.84"
-#define BINARY_VERSION 0,84,1,41
+#define BINARY_VERSION 0,84,1,42
